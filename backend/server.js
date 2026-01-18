901e1313332bed90db68fe6093b164fd73edea2b
eyualpha <eyuashenafi533@gmail.com>
2026-01-18 17:30:51 +0300

    Add environment configuration and authentication middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+import { PORT } from "./configs/env.config.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.get("/", (req, res) => {
