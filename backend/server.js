717b04b51c4c8458c5a062df056b9f0730e10bbc
eyualpha <eyuashenafi533@gmail.com>
2026-01-18 16:19:56 +0300

    Initialize backend with Express server and essential dependencies

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("HealthLink Backend is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
