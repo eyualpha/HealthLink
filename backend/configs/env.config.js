df9b6527c10b9d0d23064742469e83aa0fe02a5a
Fafi03 <138723875+Fafi03@users.noreply.github.com>
2026-01-23 08:17:09 +0300

    update

--- backend/configs/env.config.js
+++ backend/configs/env.config.js
@@ -4,4 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
 export const JWT_SECRET = process.env.JWT_SECRET;
-export const MONGO_URI = process.env.MONGODB_URI;
+export const MONGO_URI = process.env.MONGO_URI; //
